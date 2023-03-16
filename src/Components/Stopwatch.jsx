369a0bd47aa7c7196cb0c82952c177c508da921e
jallend1 <jallend1@gmail.com>
2023-03-15 20:48:53 -0700

    Refactors to make single object hold all time details

--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 const Stopwatch = () => {
-  const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
+  const [stopWatchTime, setStopWatchTime] = useState({
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
 
   const startStopwatch = () => {
     setIsRunning(true);
@@ -17,15 +21,45 @@ const Stopwatch = () => {
   const resetStopwatch = () => {
     setIsRunning(false);
     setIsPaused(false);
-    setTime(0);
+    setStopWatchTime({
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+  };
+
+  const handleTimeLogic = (prevStopWatchTime) => {
+    if (prevStopWatchTime.seconds === 59) {
+      if (prevStopWatchTime.minutes === 59) {
+        return {
+          hours: prevStopWatchTime.hours + 1,
+          minutes: 0,
+          seconds: 0,
+        };
+      } else {
+        return {
+          hours: prevStopWatchTime.hours,
+          minutes: prevStopWatchTime.minutes + 1,
+          seconds: 0,
+        };
+      }
+    } else {
+      return {
+        hours: prevStopWatchTime.hours,
+        minutes: prevStopWatchTime.minutes,
+        seconds: prevStopWatchTime.seconds + 1,
+      };
+    }
   };
 
   useEffect(() => {
     let interval = null;
     if (isRunning && !isPaused) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 10);
-      }, 10);
+        setStopWatchTime((prevStopWatchTime) => {
+          return handleTimeLogic(prevStopWatchTime);
+        });
+      }, 1000);
     } else if (!isRunning && !isPaused) {
       clearInterval(interval);
     }
@@ -42,7 +76,19 @@ const Stopwatch = () => {
 
   return (
     <div className="stopwatch">
-      <div className="time">{time}</div>
+      <div className="time">
+        {stopWatchTime.hours < 10
+          ? "0" + stopWatchTime.hours
+          : stopWatchTime.hours}
+        :
+        {stopWatchTime.minutes < 10
+          ? "0" + stopWatchTime.minutes
+          : stopWatchTime.minutes}
+        :
+        {stopWatchTime.seconds < 10
+          ? "0" + stopWatchTime.seconds
+          : stopWatchTime.seconds}
+      </div>
       <div className="buttons">
         {toggleStartButton()}
         <button onClick={resetStopwatch}>Reset</button>
