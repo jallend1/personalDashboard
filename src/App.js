6f056e036aafd433cb5583ac6ee871b8a10b0986
jallend1 <jallend1@gmail.com>
2023-03-29 21:14:56 -0700

    Removes unused component import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Stopwatch from "./Components/Stopwatch";
 import Greeting from "./Components/Greeting";
-import Toggle from "./Components/Toggle";
 import ActiveHours from "./Components/ActiveHours";
 
 function App() {
