1fad41264c462c317dea598b3664fdd4a6103bd9
jallend1 <jallend1@gmail.com>
2023-04-03 18:23:04 -0700

    Adds firebase config file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCGSRspO_rtPFJmqAVzvUvqva0l3OPZYgM",
+
+    authDomain: "personal-dashboard-76ea2.firebaseapp.com",
+
+    projectId: "personal-dashboard-76ea2",
+
+    storageBucket: "personal-dashboard-76ea2.appspot.com",
+
+    messagingSenderId: "576221293707",
+
+    appId: "1:576221293707:web:3f22c86a9c9786aa37b88e"
+
+};
+
+export default firebaseConfig;
