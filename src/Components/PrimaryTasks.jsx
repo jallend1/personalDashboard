5b0cc8ad9aea581846f5aed501c596f7d599fd89
jallend1 <jallend1@gmail.com>
2023-04-10 22:30:32 -0700

    Implements edit mode on task list

--- src/Components/PrimaryTasks.jsx
+++ src/Components/PrimaryTasks.jsx
@@ -23,6 +23,13 @@ const taskList = [
 
 const PrimaryTasks = () => {
     const [tasks, setTasks] = useState(taskList);
+    const [editMode, setEditMode] = useState(false);
+
+    const editTask = (id) => {
+        setTasks(
+            tasks.map((task) => task.id === id ? { ...task, title: 'New title' } : task)
+        );
+    }
 
     const toggleTask = (id) => {
         setTasks(
@@ -34,11 +41,28 @@ const PrimaryTasks = () => {
         <div className="task-list component">
             <header className="task-list-header">
                 <h2>Primary Tasks</h2>
-                <img src={Edit} alt="edit" />
+                <img src={Edit} alt="edit" onClick={() => setEditMode(!editMode)} />
             </header>
-            {tasks.map((task) => (
-                <Task key={task.id} task={task} onTaskClick={toggleTask} />
-            ))}
+            {editMode && (
+                <div className="edit-mode">
+                    {tasks.map((task) => (
+                        <div key={task.id} className="edit-task">
+                            <input type="text" value={task.title} onChange={(e) => editTask(task.id, e.target.value)} />
+                            <button onClick={() => editTask(task.id)}>Edit</button>
+                        </div>
+                    ))}
+                    <button onClick={() => setEditMode(false)}>Save</button>
+                    <button onClick={() => setEditMode(false)}>Cancel</button>
+                </div>
+            )}
+            {!editMode && (
+                <div className="task-list-body">
+                    {tasks.map((task) => (
+                        <Task key={task.id} task={task} onTaskClick={toggleTask} />
+                    ))}
+                </div>
+            )}
+
         </div>
     )
 }
