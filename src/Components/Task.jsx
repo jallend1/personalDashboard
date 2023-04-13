9752debbba230e302c4592f65b63ac00751abf98
jallend1 <jallend1@gmail.com>
2023-04-12 19:32:12 -0700

    Adjusts background color on completed and incomplete tasks

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -2,7 +2,7 @@ const Task = ({ task = { title: 'Sample task', completed: false }, onTaskClick }
     return (
         <div
             className="task"
-            style={{ backgroundColor: task.completed ? "green" : "red" }}
+            style={{ backgroundColor: task.completed ? "rgb(13, 142, 68, 0.3)" : "rgb(231, 64, 50, 0.3)" }}
             onClick={() => onTaskClick(task.id)}
         >
             <input type="checkbox" checked={task.completed} onChange={() => onTaskClick(task.id)} />
